3e972972a87394b06e36b701dc92763bda2cd502
Linner <45491119+LLLLLLer@users.noreply.github.com>
2024-09-08 01:36:19 +0800

    Update weibo_sign_chaohua.py

--- weibo_sign_chaohua.py
+++ weibo_sign_chaohua.py
@@ -9,8 +9,8 @@ from datetime import datetime, timedelta
 
 API_URL = "https://api.weibo.cn/2/cardlist"
 SIGN_URL = "https://api.weibo.cn/2/page/button"
-weibo_my_cookie = "https://api.weibo.cn/2/cardlist?gsid=_2A25L3cd1DeRxGeNN4lIT9S3KyzWIHXVmy129rDV6PUJbkdANLUPjkWpNSZIzhy-1vEAWvOTW2OzRlH3i1i83YEvz&wm=3333_2001&launchid=10000365--x&b=0&from=10D1093010&c=iphone&networktype=wifi&v_p=90&skin=default&v_f=1&s=6adc3325&lang=zh_CN&sflag=1&ua=iPhone12,1__weibo__13.1.0__iphone__os16.4.1&ft=0&aid=01A_H3R1DJltBVOq43_YwyFz-i-tgKOfKgn-iJm1YlPbhl2kk.&lcardid=&sg_page_search=1&luicode=10000011&orifid=profile_me%24%24231093_-_chaohua&sg_card201_2_enable=1&containerid=232478_-_bottom_mine_followed&sourcetype=page&sg_one_checkin_enable=1&sgtotal_activity_enable=1&sg_cp_template_enable=1&need_head_cards=0&pd_redpacket2022_enable=1&sg_wish2_enable=1&sg_page_tab_wbox_enable=1&is_push_alert=1&sgpage_newprofile_enable=1&enable_card214_hot_discuss=1&need_new_pop=1&card210_grade_enable=1&profile_toolbar_refactor=1&is_auto_scroll=1&card199_realtime_enable=1&client_key=82933924bf998e81f9e3172ab92d3555&lfid=231093_-_chaohua&card199_match_info_enable=1&count=20&is_winter_olympics_enable=1&sg_diamond_skin_enable=1&sg_airborne_theme_enable=1&card182_schedule_enable=1&page=1&uicode=10001387&card204_enable=1&tz=Asia%2FShanghai&supergroup_album_v2=1&card196_videolive_enable=1&sys_notify_open=0&card211_enable=1&sg_wishing_well_enable=1&refresh_type=0&oriuicode=10000011_10000011&mix_media_enable=1&st_bottom_bar_new_style_enable=1&support_switch_sort_enable=1&fid=232478_-_bottom_mine_followed&sg_search_person_topic_card_c60_enable=1&image_type=heif&card159164_emoji_enable=1&sg_page_header_v2=1&moduleID=pagecard&ul_sid=7BA35572-C991-4658-A799-BF6C550072B8&ul_hid=8242FC08-D2D6-4967-8D4F-1F7E983831A4&ul_ctime=1725554049738"
-STREAK_FILE = "sign_in_streak.txt"
+# weibo_my_cookie = "https://api.weibo.cn/2/cardlist?gsid=_2A25L3cd1DeRxGeNN4lIT9S3KyzWIHXVmy129rDV6PUJbkdANLUPjkWpNSZIzhy-1vEAWvOTW2OzRlH3i1i83YEvz&wm=3333_2001&launchid=10000365--x&b=0&from=10D1093010&c=iphone&networktype=wifi&v_p=90&skin=default&v_f=1&s=6adc3325&lang=zh_CN&sflag=1&ua=iPhone12,1__weibo__13.1.0__iphone__os16.4.1&ft=0&aid=01A_H3R1DJltBVOq43_YwyFz-i-tgKOfKgn-iJm1YlPbhl2kk.&lcardid=&sg_page_search=1&luicode=10000011&orifid=profile_me%24%24231093_-_chaohua&sg_card201_2_enable=1&containerid=232478_-_bottom_mine_followed&sourcetype=page&sg_one_checkin_enable=1&sgtotal_activity_enable=1&sg_cp_template_enable=1&need_head_cards=0&pd_redpacket2022_enable=1&sg_wish2_enable=1&sg_page_tab_wbox_enable=1&is_push_alert=1&sgpage_newprofile_enable=1&enable_card214_hot_discuss=1&need_new_pop=1&card210_grade_enable=1&profile_toolbar_refactor=1&is_auto_scroll=1&card199_realtime_enable=1&client_key=82933924bf998e81f9e3172ab92d3555&lfid=231093_-_chaohua&card199_match_info_enable=1&count=20&is_winter_olympics_enable=1&sg_diamond_skin_enable=1&sg_airborne_theme_enable=1&card182_schedule_enable=1&page=1&uicode=10001387&card204_enable=1&tz=Asia%2FShanghai&supergroup_album_v2=1&card196_videolive_enable=1&sys_notify_open=0&card211_enable=1&sg_wishing_well_enable=1&refresh_type=0&oriuicode=10000011_10000011&mix_media_enable=1&st_bottom_bar_new_style_enable=1&support_switch_sort_enable=1&fid=232478_-_bottom_mine_followed&sg_search_person_topic_card_c60_enable=1&image_type=heif&card159164_emoji_enable=1&sg_page_header_v2=1&moduleID=pagecard&ul_sid=7BA35572-C991-4658-A799-BF6C550072B8&ul_hid=8242FC08-D2D6-4967-8D4F-1F7E983831A4&ul_ctime=1725554049738"
+weibo_my_cookie = "https://api.weibo.cn/2/cardlist?gsid=_2A25L2PvgDeRxGeFM7lER-C3Jzz6IHXVmzAgorDV6PUJbkdANLXP1kWpNQN6a0EXgp1rb6_s9X9Sc8d_1LBRl_lRz&wm=3333_2001&launchid=10000365--x&b=0&from=10D1093010&c=iphone&networktype=wifi&v_p=90&skin=default&v_f=1&s=9a8f945c&lang=zh_CN&sflag=1&ua=iPhone12,1__weibo__13.1.0__iphone__os16.4.1&ft=0&aid=01A_H3R1DJltBVOq43_YwyFz-i-tgKOfKgn-iJm1YlPbhl2kk.&lcardid=&luicode=10000011&orifid=profile_me%24%24231093_-_chaohua&sg_page_search=1&sg_card201_2_enable=1&page_interrupt_enable=1&containerid=232478_-_bottom_mine_usual_visit&sourcetype=page&sg_one_checkin_enable=1&sgtotal_activity_enable=1&sg_cp_template_enable=1&need_head_cards=0&pd_redpacket2022_enable=1&sg_wish2_enable=1&sg_page_tab_wbox_enable=1&is_push_alert=1&sgpage_newprofile_enable=1&enable_card214_hot_discuss=1&need_new_pop=1&card210_grade_enable=1&profile_toolbar_refactor=1&is_auto_scroll=1&card199_realtime_enable=1&client_key=82933924bf998e81f9e3172ab92d3555&lfid=231093_-_chaohua&card199_match_info_enable=1&count=20&is_winter_olympics_enable=1&sg_diamond_skin_enable=1&sg_airborne_theme_enable=1&card182_schedule_enable=1&page=1&uicode=10001387&card204_enable=1&tz=Asia%2FShanghai&supergroup_album_v2=1&card196_videolive_enable=1&sys_notify_open=0&card211_enable=1&sg_wishing_well_enable=1&refresh_type=0&oriuicode=10000011_10000011&mix_media_enable=1&st_bottom_bar_new_style_enable=1&support_switch_sort_enable=1&fid=232478_-_bottom_mine_usual_visit&sg_search_person_topic_card_c60_enable=1&image_type=heif&card159164_emoji_enable=1&sg_page_header_v2=1&moduleID=pagecard&ul_sid=D026B342-AB6C-4829-B68B-4D14975A1BE7&ul_hid=D026B342-AB6C-4829-B68B-4D14975A1BE7&ul_ctime=1725729914618"
 
 def send_request(url, params, headers):
     response = requests.get(url, params=params, headers=headers)
@@ -119,40 +119,35 @@ def sign_in(headers, base_params, scheme):
     return data
 
 def read_streak():
-    if os.path.exists(STREAK_FILE):
-        with open(STREAK_FILE, 'r') as f:
-            content = f.read().strip()
-            if content:
-                last_date_str, streak_count = content.split(',')
-                last_date = datetime.strptime(last_date_str, "%Y-%m-%d")
-                return last_date, int(streak_count)
+    # 从 GitHub Actions 环境变量读取 streak
+    STREAK_HISTORY = os.getenv('STREAK_HISTORY', 'None')
+    if STREAK_HISTORY != 'None':
+        last_date_str, streak_count = STREAK_HISTORY.split(',')
+        last_date = datetime.strptime(last_date_str, "%Y-%m-%d")
+        return last_date, int(streak_count)
     return None, 0
 
 def update_streak(today, streak_count):
-    with open(STREAK_FILE, 'w') as f:
-        f.write(f"{today.strftime('%Y-%m-%d')},{streak_count}")
+    # 更新 GitHub Actions 环境变量
+    with open(os.getenv('GITHUB_ENV'), 'a') as f:
+        f.write(f"STREAK_HISTORY={today.strftime('%Y-%m-%d')},{streak_count}\n")
 
 
-headers = {
-    "Accept": "*/*",
-    "User-Agent": "Weibo/81434 (iPhone; iOS 17.0; Scale/3.00)",
-    "SNRT": "normal",
-    "X-Sessionid": "6AFD786D-9CFA-4E18-BD76-60D349FA8CA2",
-    "Accept-Encoding": "gzip, deflate",
-    "X-Validator": "QTDSOvGXzA4i8qLXMKcdkqPsamS5Ax1wCJ42jfIPrNA=",
-    "Host": "api.weibo.cn",
-    "x-engine-type": "cronet-98.0.4758.87",
-    "Connection": "keep-alive",
-    "Accept-Language": "en-US,en",
-    "cronet_rid": "6524001",
-    "Authorization": "",
-    "X-Log-Uid": "5036635027",
-}
+user_agents = [
+    "Weibo/81434 (iPhone; iOS 16.0; Scale/3.00)",
+    "Weibo/81435 (iPhone; iOS 17.0; Scale/2.00)",
+    "Weibo/81436 (iPhone; iOS 17.1; Scale/3.00)"
+]
+
+headers = {"Accept": "*/*", "SNRT": "normal", "X-Sessionid": "8B83D45D-A5A4-4006-A9C2-1AB5452AB4FD",
+           "Accept-Encoding": "gzip, deflate", "X-Validator": "nsYeah7o/zOlMt4Lhnz6YCNBJ4v1Kh1eT2Pr3Bh0tRo=",
+           "Host": "api.weibo.cn", "x-engine-type": "cronet-98.0.4758.87", "Connection": "keep-alive",
+           "Accept-Language": "en-US,en", "cronet_rid": "6524001", "Authorization": "", "X-Log-Uid": "7253083542",
+           "User-Agent": random.choice(user_agents)}
 
 if __name__ == "__main__":
     # weibo_my_cookie = ''
     params = extract_params(weibo_my_cookie)
-    # params = extract_params(os.getenv("weibo_my_cookie"))
 
     # 获取超话列表
     card_type_11_info = get_card_type_11(params, headers)
